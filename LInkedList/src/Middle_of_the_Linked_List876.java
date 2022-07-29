fce938a980c88c8b07258b3cb76562bdee589d82
jh770 <jh770@duke.edu>
2022-07-29 01:01:45 -0400

    777

---
+++ LInkedList/src/Middle_of_the_Linked_List876.java
@@ -0,0 +1,14 @@
+public class Middle_of_the_Linked_List876 {
+    public ListNode middleNode(ListNode head) {
+       //快慢指针，快指针走两步，慢指针走一步，当快指针到终点时，慢指针正好到一半位置
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+        return slow;
+    }
+}
